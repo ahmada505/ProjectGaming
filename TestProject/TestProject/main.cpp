9d17e277f6a71d442b7ac43e5b9331ac185ba430
Aness Ahmad <anessahmad20@gmail.com>
2018-11-08 00:37:07 +0100

    added main to project

---
+++ TestProject/TestProject/main.cpp
@@ -0,0 +1,42 @@
+#include "main.h"
+
+#include<iostream>
+#include <time.h>
+#include "assert.h"
+
+using namespace std;
+
+
+int main()
+{
+
+	while (true) {
+
+		int first;
+		int sec;
+		int thrid;
+		int fourth;
+
+
+		//int array [min];
+
+		// Prompt user for two integer values to specify a range.
+		cout << "Enter minimum integer value of number range: ";
+		cin >> first;
+		cout << "Enter maximum integer value of number range: ";
+		cin >> sec;
+
+
+	}
+}
+
+
+main::main()
+{
+
+}
+
+
+main::~main()
+{
+}
