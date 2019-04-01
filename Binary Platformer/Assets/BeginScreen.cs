e0120d47a3cc2c2c129515c2f75bb6ec89833de3
Aness Ahmad <anessahmad20@gmail.com>
2019-04-01 10:52:13 +0200

    made a build of the game few changes to level layout made a gameover- and win screen

---
+++ Binary Platformer/Assets/BeginScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeginScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleInput();
+    }
+
+    private void handleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel("BSP dungeon");
+        }
+
+    }
+}
---
+++ Binary Platformer/Assets/GameOverScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleInput();
+	}
+
+
+    private void handleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel("BSP dungeon");
+        }
+
+    }
+}
